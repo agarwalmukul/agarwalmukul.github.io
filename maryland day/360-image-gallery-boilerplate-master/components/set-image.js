a5d19db5a81998757f2f69327ec748d021479ade
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-03-29 12:09:06 -0400

    change both images test 2.2

--- maryland day/360-image-gallery-boilerplate-master/components/set-image.js
+++ maryland day/360-image-gallery-boilerplate-master/components/set-image.js
@@ -25,7 +25,7 @@ AFRAME.registerComponent('set-image', {
       setTimeout(function () {
         // Set image.
         data.target.setAttribute('material', 'src', data.src);
-        document.querySelector("#tex2").setAttribute('material', 'src', 'tex'+eval(data.src.charAt(3)+'+ 1')  )
+        document.querySelector("#right-eye").setAttribute('material', 'src', 'tex'+eval(data.src.charAt(3)+'+ 1')  )
       }, data.dur);
     });
   },
