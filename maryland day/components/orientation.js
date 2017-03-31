bbd8696f76db5c17f2ab3081e54c59495eae827f
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-03-31 18:12:55 -0400

    ballerina view change

--- maryland day/components/orientation.js
+++ maryland day/components/orientation.js
@@ -6,8 +6,8 @@ var ball   = document.querySelector('#left-eye');
 //var maxY = garden.clientHeight - ball.clientHeight;
 
 function handleOrientation(event) {
-  var x = event.beta;  // In degree in the range [-180,180]
-  var y = event.gamma; // In degree in the range [-90,90]
+  var xpos = event.beta;  // In degree in the range [-180,180]
+  var ypos = event.gamma; // In degree in the range [-90,90]
 
  // output.innerHTML  = "beta : " + x + "\n";
 //  output.innerHTML += "gamma: " + y + "\n";
@@ -19,12 +19,12 @@ function handleOrientation(event) {
 
   // To make computation easier we shift the range of 
   // x and y to [0,180]
-  x += 90;
-  y += 90;
+  xpos += 90;
+  ypos += 90;
 
   // 10 is half the size of the ball
   // It center the positioning point to the center of the ball
-  ball.setAttribute('position', {x: x/9, y: y/9; z: -4});
+  ball.setAttribute('position', {x: xpos/9, y: ypos/9; z: -4});
   //ball.style.top  = (maxX*x/180 - 10) + "px";
   //ball.style.left = (maxY*y/180 - 10) + "px";
 }
