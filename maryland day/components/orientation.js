9d738706c37e7f14823bb66c462208a3846f246b
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-03-31 21:59:16 -0400

    ballerina view change

--- maryland day/components/orientation.js
+++ maryland day/components/orientation.js
@@ -1,6 +1,7 @@
 function orientation(){
 
-var ball= document.querySelector('#left-eye');
+var left= document.querySelector('#left-eye');
+var right= document.querySelector('#right-eye');
 
 
 function handleOrientation(event) {
@@ -8,15 +9,15 @@ function handleOrientation(event) {
   var zpos = event.alpha; // In degree in the range [0,360]
 
   xpos += 180;
-  ypos-=180
+  //ypos-=180
   //ypos += 90;
 
-  var image_num = Math.floor((zpos/20)%7);
+  var image_num = Math.floor((zpos/10)%7)+1;
 
   //ball.setAttribute('position', {x: 1.75, y: 1.25+image_num, z: -4});
-        document.querySelector("#left-eye").setAttribute('material', 'src', "#tex" + image_num.toString() );
+        left.setAttribute('material', 'src', "#tex" + image_num.toString() );
         //document.querySelector("#right-eye").setAttribute('material', 'src', 'tex'+eval(data.src.charAt(3)+'+ 1')  )
-        document.querySelector("#right-eye").setAttribute('material', 'src', "#tex" + (image_num+1).toString() );
+        right.setAttribute('material', 'src', "#tex" + (image_num+1).toString() );
 
 
 }
