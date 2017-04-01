71f07a5be9a203d48644d1397123db9ea86dd705
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-03-31 20:44:12 -0400

    ballerina view change

--- maryland day/components/orientation.js
+++ maryland day/components/orientation.js
@@ -1,6 +1,8 @@
+function orientation(){
+
 var ball= document.querySelector('#left-eye');
 
-}
+
 function handleOrientation(event) {
   var xpos = event.beta;  // In degree in the range [-180,180]
   var ypos = event.gamma; // In degree in the range [-90,90]
@@ -8,7 +10,8 @@ function handleOrientation(event) {
   xpos += 90;
   ypos += 90;
 
-  ball.setAttribute('position', {x: xpos/9, y: ypos/9, z: -4});
+  ball.setAttribute('position', {x: 1.75+(xpos/90), y: 1.25+(ypos/90), z: -4});
 }
 
 window.addEventListener('deviceorientation', handleOrientation);
+}
