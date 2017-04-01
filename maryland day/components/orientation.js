c97089e491a520b78b140f1bbbf6ae5029a5fedd
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-03-31 20:36:18 -0400

    ballerina view change

--- maryland day/components/orientation.js
+++ maryland day/components/orientation.js
@@ -1,32 +1,14 @@
-var ball   = document.querySelector('#left-eye');
-//var garden = document.querySelector('.garden');
-//var output = document.querySelector('.output');
+var ball= document.querySelector('#left-eye');
 
-//var maxX = garden.clientWidth  - ball.clientWidth;
-//var maxY = garden.clientHeight - ball.clientHeight;
-
+}
 function handleOrientation(event) {
   var xpos = event.beta;  // In degree in the range [-180,180]
   var ypos = event.gamma; // In degree in the range [-90,90]
 
- // output.innerHTML  = "beta : " + x + "\n";
-//  output.innerHTML += "gamma: " + y + "\n";
-
-  // Because we don't want to have the device upside down
-  // We constrain the x value to the range [-90,90]
- // if (x >  90) { x =  90};
- // if (x < -90) { x = -90};
-
-  // To make computation easier we shift the range of 
-  // x and y to [0,180]
   xpos += 90;
   ypos += 90;
 
-  // 10 is half the size of the ball
-  // It center the positioning point to the center of the ball
-  ball.setAttribute('position', {x: xpos/9, y: ypos/9; z: -4});
-  //ball.style.top  = (maxX*x/180 - 10) + "px";
-  //ball.style.left = (maxY*y/180 - 10) + "px";
+  ball.setAttribute('position', {x: xpos/9, y: ypos/9, z: -4});
 }
 
 window.addEventListener('deviceorientation', handleOrientation);
