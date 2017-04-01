a3ca3d26af7404934a8ada7efd5034e3ab37e82c
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-03-31 22:26:08 -0400

    ballerina view change

--- maryland day/components/orientation.js
+++ maryland day/components/orientation.js
@@ -1,5 +1,5 @@
 var image_num=0;
-
+var static=0;
 function orientation(){
 
 var left= document.querySelector('#left-eye');
@@ -16,8 +16,13 @@ function handleOrientation(event) {
   //ypos += 90;
 
   var temp = Math.floor((zpos/3)%7)+1;
+  if(static==0 && temp!=0){
+    static = temp;
+  }
+  if(static!=temp){
   if(temp>image_num){image_num++;}
   else{image_num--;}
+}
 
   //ball.setAttribute('position', {x: 1.75, y: 1.25+image_num, z: -4});
         left.setAttribute('material', 'src', "#tex" + image_num.toString() );
