0597adddf6c1850dd2c898fce6c9939c1e789786
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-04-05 13:50:28 -0400

    ballerina stereo mode ui handling and image change from 7 to 6 instead of 7 to 1

--- maryland day/components/orientation.js
+++ maryland day/components/orientation.js
@@ -1,9 +1,7 @@
-
 function orientation(){
 
 var left= document.querySelector('#left-eye');
 var right= document.querySelector('#right-eye');
-/*
 setTimeout(function(){
  var orientationModal = document.querySelector(".a-orientation-modal");
   if(orientationModal!=null){
@@ -11,8 +9,6 @@ setTimeout(function(){
       //orientationModal.className += " a-hidden";
   } 
 }, 5000);
-*/
- }
  
 function handleOrientation(event) {
   var xpos = event.beta;  // In degree in the range [-180,180]
@@ -29,35 +25,14 @@ function handleOrientation(event) {
   //ypos += 90;
 
   var image_num = Math.floor((zpos/3)%7)+1;
-  /*
-   var image_num = Math.floor((zpos/3)%13)+1;
-  if(image_num>7){
-    image_num=14-image_num;
-  }
-*/
+
   //ball.setAttribute('position', {x: 1.75, y: 1.25+image_num, z: -4});
         left.setAttribute('material', 'src', "#tex" + image_num.toString() );
         //document.querySelector("#right-eye").setAttribute('material', 'src', 'tex'+eval(data.src.charAt(3)+'+ 1')  )
         right.setAttribute('material', 'src', "#tex" + (image_num+1).toString() );
+
+
 }
 
 window.addEventListener('deviceorientation', handleOrientation);
-
-window.addEventListener("orientationchange", function(){
-  applyOrientation();
-}, false);
-
-function applyOrientation() {
-  if (window.innerHeight > window.innerWidth) {
-   //alert("You are now in portrait");
-  } else {
-    //alert("You are now in landscape");
-    setTimeout(function(){
-       var orientationModal = document.querySelector(".a-orientation-modal");
-        if(orientationModal!=null){
-            //orientationModal.parentNode.removeChild(orientationModal);
-            orientationModal.className += " a-hidden";
-        } 
-      }, 0);
-  }
 }
