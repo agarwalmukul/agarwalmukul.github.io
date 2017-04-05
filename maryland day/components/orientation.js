182efdc0ae39033e5efabcf558a4f68683a69d5c
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-04-05 13:32:50 -0400

    ballerina stereo mode ui handling and image change from 7 to 6 instead of 7 to 1

--- maryland day/components/orientation.js
+++ maryland day/components/orientation.js
@@ -1,3 +1,4 @@
+/*
 function orientation(){
 
 var left= document.querySelector('#left-eye');
@@ -9,7 +10,12 @@ setTimeout(function(){
       //orientationModal.className += " a-hidden";
   } 
 }, 5000);
- 
+
+ }
+ */
+//var image_num = 1;
+//bool increase = true;
+
 function handleOrientation(event) {
   var xpos = event.beta;  // In degree in the range [-180,180]
   var zpos = event.alpha; // In degree in the range [0,360]
@@ -18,21 +24,41 @@ function handleOrientation(event) {
   //changed the front view point in the scene from 0, 360 discontinuity to continuous
   zpos-=180;
   if(zpos<0){
-    zpos= 360 + zpos;
+    zpos = 360 + zpos;
   }
   //zpos+=180;
   //ypos-=180
   //ypos += 90;
 
-  var image_num = Math.floor((zpos/3)%7)+1;
+  //var image_num = Math.floor((zpos/3)%7)+1;
+  var image_num = Math.floor((zpos/3)%13)+1;
+  if(image_num>7){
+    image_num=14-image_num;
+  }
 
   //ball.setAttribute('position', {x: 1.75, y: 1.25+image_num, z: -4});
         left.setAttribute('material', 'src', "#tex" + image_num.toString() );
         //document.querySelector("#right-eye").setAttribute('material', 'src', 'tex'+eval(data.src.charAt(3)+'+ 1')  )
         right.setAttribute('material', 'src', "#tex" + (image_num+1).toString() );
-
-
 }
-window.onload = orientation;
+
 window.addEventListener('deviceorientation', handleOrientation);
+
+window.addEventListener("orientationchange", function(){
+  applyOrientation();
+}, false);
+
+function applyOrientation() {
+  if (window.innerHeight > window.innerWidth) {
+   //alert("You are now in portrait");
+  } else {
+    //alert("You are now in landscape");
+    setTimeout(function(){
+       var orientationModal = document.querySelector(".a-orientation-modal");
+        if(orientationModal!=null){
+            //orientationModal.parentNode.removeChild(orientationModal);
+            orientationModal.className += " a-hidden";
+        } 
+      }, 0);
+  }
 }
