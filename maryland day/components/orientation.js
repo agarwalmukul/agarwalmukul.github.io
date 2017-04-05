9c17c16665c69a7b59bf7696f229fcd6fbbc3a3f
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-04-05 14:19:57 -0400

    ballerina stereo mode ui handling and image change from 7 to 6 instead of 7 to 1

--- maryland day/components/orientation.js
+++ maryland day/components/orientation.js
@@ -2,7 +2,7 @@
 function orientation(){
 var left= document.querySelector('#left-eye');
 var right= document.querySelector('#right-eye');
-var increase = false;
+//var increase = false;
  
 function handleOrientation(event) {
   var xpos = event.beta;  // In degree in the range [-180,180]
@@ -10,6 +10,7 @@ function handleOrientation(event) {
 
   xpos += 180;
   //changed the front view point in the scene from 0, 360 discontinuity to continuous
+  /*
   zpos-=180;
   if(zpos<0){
     zpos = 360 + zpos;
@@ -37,13 +38,13 @@ function handleOrientation(event) {
       image_num=temp--;
     }
   }
+  */
   
-  /*
   var image_num = Math.floor((zpos/3)%13)+1;
   if(image_num>7){
     image_num=14-image_num;
   }
-*/
+
   left.setAttribute('material', 'src', "#tex" + image_num.toString() );
   //document.querySelector("#right-eye").setAttribute('material', 'src', 'tex'+eval(data.src.charAt(3)+'+ 1')  )
   right.setAttribute('material', 'src', "#tex" + (image_num+1).toString() );
