3a4cc2d449ce4fa4bfd97a9300e4f2e56c612524
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-04-05 13:54:16 -0400

    ballerina stereo mode ui handling and image change from 7 to 6 instead of 7 to 1

--- maryland day/components/orientation.js
+++ maryland day/components/orientation.js
@@ -1,14 +1,8 @@
-function orientation(){
 
+function orientation(){
 var left= document.querySelector('#left-eye');
 var right= document.querySelector('#right-eye');
-setTimeout(function(){
- var orientationModal = document.querySelector(".a-orientation-modal");
-  if(orientationModal!=null){
-      orientationModal.parentNode.removeChild(orientationModal);
-      //orientationModal.className += " a-hidden";
-  } 
-}, 5000);
+
  
 function handleOrientation(event) {
   var xpos = event.beta;  // In degree in the range [-180,180]
@@ -20,19 +14,36 @@ function handleOrientation(event) {
   if(zpos<0){
     zpos = 360 + zpos;
   }
-  //zpos+=180;
-  //ypos-=180
-  //ypos += 90;
 
   var image_num = Math.floor((zpos/3)%7)+1;
-
-  //ball.setAttribute('position', {x: 1.75, y: 1.25+image_num, z: -4});
-        left.setAttribute('material', 'src', "#tex" + image_num.toString() );
-        //document.querySelector("#right-eye").setAttribute('material', 'src', 'tex'+eval(data.src.charAt(3)+'+ 1')  )
-        right.setAttribute('material', 'src', "#tex" + (image_num+1).toString() );
-
-
+  /*
+   var image_num = Math.floor((zpos/3)%13)+1;
+  if(image_num>7){
+    image_num=14-image_num;
+  }
+*/
+  left.setAttribute('material', 'src', "#tex" + image_num.toString() );
+  //document.querySelector("#right-eye").setAttribute('material', 'src', 'tex'+eval(data.src.charAt(3)+'+ 1')  )
+  right.setAttribute('material', 'src', "#tex" + (image_num+1).toString() );
+}
+window.addEventListener('deviceorientation', handleOrientation);
 }
 
-window.addEventListener('deviceorientation', handleOrientation);
+window.addEventListener("orientationchange", function(){
+  applyOrientation();
+}, false);
+
+function applyOrientation() {
+  if (window.innerHeight > window.innerWidth) {
+   //alert("You are now in portrait");
+  } else {
+    //alert("You are now in landscape");
+    setTimeout(function(){
+       var orientationModal = document.querySelector(".a-orientation-modal");
+        if(orientationModal!=null){
+            //orientationModal.parentNode.removeChild(orientationModal);
+            orientationModal.className += " a-hidden";
+        } 
+      }, 0);
+  }
 }
