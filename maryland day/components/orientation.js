5f29ca757464cbfad46bf397ca4b9480c9fa4d0a
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-04-14 15:40:11 -0400

    ballstereo mode camera rotation between portrait and landscape mode

--- maryland day/components/orientation.js
+++ maryland day/components/orientation.js
@@ -3,42 +3,13 @@ function orientation(){
 var left= document.querySelector('#left-eye');
 var right= document.querySelector('#right-eye');
 //var increase = false;
- 
+/*
 function handleOrientation(event) {
   var xpos = event.beta;  // In degree in the range [-180,180]
   var zpos = event.alpha; // In degree in the range [0,360]
 
   xpos += 180;
   //changed the front view point in the scene from 0, 360 discontinuity to continuous
-  /*
-  zpos-=180;
-  if(zpos<0){
-    zpos = 360 + zpos;
-  }
-  
-  var temp = image_num;
-  var image_num = Math.floor((zpos/3)%7)+1;
-  if(image_num!=temp){
-    if(temp<image_num){
-      increase = true;
-    }
-    else{
-      increase=false;
-    }
-    if(temp==7 && image_num==1){
-      increase = false;
-    }
-    if(temp==1 && image_num==7){
-      increase = true;
-    }
-    if(increase){
-      image_num=temp++;
-    }
-    else{
-      image_num=temp--;
-    }
-  }
-  */
   var width = 360/(28*7);
   var x=Math.floor((zpos/width)/7);
   var x = x % 2;
@@ -56,6 +27,8 @@ function handleOrientation(event) {
   //document.querySelector("#right-eye").setAttribute('material', 'src', 'tex'+eval(data.src.charAt(3)+'+ 1')  )
   right.setAttribute('material', 'src', "#tex" + (image_num+1).toString() );
 }
+
+*/
 //window.addEventListener('deviceorientation', handleOrientation);
 }
 
