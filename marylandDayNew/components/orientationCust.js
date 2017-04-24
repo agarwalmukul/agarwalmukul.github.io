599197d1e61e6e152ecd461c2fa6b7015fef3563
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-04-24 16:57:37 -0400

    university logo in vr mode

--- marylandDayNew/components/orientationCust.js
+++ marylandDayNew/components/orientationCust.js
@@ -4,5 +4,5 @@ var width = document.documentElement.clientWidth;
 var height = document.documentElement.clientHeight;
 stereoLogo.setAttribute('width'," "+width/10000);
 stereoLogo.setAttribute('height'," "+width/10000);
-stereoLogo.setAttribute('position',"-"+width*4/10000+ " -"+height*4/10000+" -0.25");
+stereoLogo.setAttribute('position',"-"+width*2/10000+ " -"+height*2.5/10000+" -0.25");
 }
