2bc679cf6b3272f0e9672bf7cf977263d5874a6b
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-04-25 12:38:51 -0400

    stereo mode for different screen size

--- marylandDayNew/components/orientationCust.js
+++ marylandDayNew/components/orientationCust.js
@@ -47,6 +47,15 @@ function handleOrientation(event) {
         //vrbutton.click();
         //enteredVR = true;
         isLandscapeVRModeFirst = true;
+        setTimeout(function(){ 
+          //alert("Hello");
+          var widthScreen = document.documentElement.clientWidth;
+          var widthcanvas = parseInt(document.querySelector('.a-canvas').style.width)/2;
+          //document.querySelector('.a-canvas').style.width = widthcanvas;
+          document.querySelector('.a-canvas').style.width = widthcanvas * 1.07;
+          document.querySelector('.a-canvas').style.right = widthcanvas * (-0.07/2);
+        }, 300);
+      
       }
     }
   // to automatically exit vr mode on portrait mode - for android
