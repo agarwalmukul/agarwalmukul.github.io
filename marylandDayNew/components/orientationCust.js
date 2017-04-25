bb73b70a2c19a27149e025f1ec7522b6dff03468
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-04-24 20:47:01 -0400

    university logo in vr mode

--- marylandDayNew/components/orientationCust.js
+++ marylandDayNew/components/orientationCust.js
@@ -5,7 +5,16 @@ var width = document.documentElement.clientWidth;
 var height = document.documentElement.clientHeight;
 stereoLogo.setAttribute('width'," "+width/10000);
 stereoLogo.setAttribute('height'," "+width/10000);
-stereoLogo.setAttribute('position',"-"+width*6/10000+ " -"+height*2.5/10000+" -0.25");
+var position = " ";
+//position+= (width*-2.5/10000);
+position+= (width*-6.4/10000);
+position+= " ";
+position+= (height*-5.5/10000);
+//position+= 0;
+position+= " -0.25";
+//stereoLogo.setAttribute('position'," " + (width*-2.5/10000) + " "+ (height*-2.5/10000) + " -0.25");
+stereoLogo.setAttribute('position'," " +position);
+//stereoLogo.setAttribute('position',"0 0 -0.25");
 
 var enteredVR = false;
 
@@ -16,9 +25,6 @@ function handleOrientation(event) {
   if(Math.abs(xpos)<10 || Math.abs(xpos)>170){
       if(!enteredVR){
         //document.querySelector('a-scene').enterVR();
-        var width = document.documentElement.clientWidth;
-        var height = document.documentElement.clientHeight;
-        stereoLogo.setAttribute('position',"-"+width*2.5/10000+ " -"+height*2.5/10000+" -0.25");
         stereoLogo.setAttribute('material',"opacity","1");
         logo.style.opacity =0;
         //vrbutton.click();
