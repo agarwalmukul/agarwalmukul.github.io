30c55e33d169602ea588e9a868b1d4bb88d0215c
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-04-24 23:39:54 -0400

    university logo in vr mode

--- marylandDayNew/components/orientationCust.js
+++ marylandDayNew/components/orientationCust.js
@@ -34,16 +34,16 @@ function handleOrientation(event) {
   var ypos = event.gamma; // In degree in the range [0,360]
   var zpos = event.alpha; // In degree in the range [0,360]
   
-  //if(Math.abs(xpos)<10 || Math.abs(xpos)>170){
-      //if(!enteredVR){
+  if(Math.abs(xpos)<10 || Math.abs(xpos)>170){
+      if(!enteredVR){
         //document.querySelector('a-scene').enterVR();
-        //stereoLogo.setAttribute('material',"opacity","1");
+        stereoLogo.setAttribute('material',"opacity","1");
         //stereoLogo.setAttribute('position',"-"+width*5.5/10000+ " -"+height*5/10000+" -0.25");
-        //logo.style.opacity =0;
+        logo.style.opacity =0;
         //vrbutton.click();
-        //enteredVR = true;
-      //}
-    //}
+        enteredVR = true;
+      }
+    }
   
   if(Math.abs(xpos)>60 && Math.abs(xpos)<120){
     if(enteredVR){
@@ -54,8 +54,8 @@ function handleOrientation(event) {
     }
   }
 }
-
-window.addEventListener('orientationchange', doOnOrientationChange);
+if(getMobileOperatingSystem()=="iOS"){
+window.addEventListener('orientationchange', doOnOrientationChange);}
 
 document.querySelector('a-scene').addEventListener('enter-vr', function () {
    //document.querySelector('a-scene').enterVR();
@@ -64,6 +64,35 @@ document.querySelector('a-scene').addEventListener('enter-vr', function () {
         //vrbutton.click();
         enteredVR = true;
 });
+document.querySelector('a-scene').addEventListener('exit-vr', function () {
+   //document.querySelector('a-scene').enterVR();
+        //stereoLogo.setAttribute('material',"opacity","1");
+        logo.style.opacity =1;
+        //vrbutton.click();
+        enteredVR = false;
+});
 window.addEventListener('deviceorientation', handleOrientation);
 
+}
+
+
+
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "unknown";
 }
