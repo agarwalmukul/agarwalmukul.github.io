b970df03fa54f97a71eefc57950fd6324bd2a67c
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-04-24 23:11:31 -0400

    university logo in vr mode

--- marylandDayNew/components/orientationCust.js
+++ marylandDayNew/components/orientationCust.js
@@ -6,8 +6,28 @@ var height = document.documentElement.clientHeight;
 var stereoLogoScale = Math.min(width, height)/10000;
 stereoLogo.setAttribute('width'," "+stereoLogoScale);
 stereoLogo.setAttribute('height'," "+stereoLogoScale);
-stereoLogo.setAttribute('position',"-"+width*2.5/10000+ " -"+height*2.5/10000+" -0.25");
+stereoLogo.setAttribute('position',"-"+Math.min(width, height)*2.5/10000+ " -"+Math.max(width, height)*2.5/10000+" -0.25");
+
+var minLength = Math.min(width, height);
+logo.style.width = minLength/5 + "px";
 var enteredVR = false;
+
+function doOnOrientationChange() {
+    switch(window.orientation) {  
+      case -90 || 90:
+        //alert('landscape');
+        //logo.style.width = minLength/5 + "px";
+        break;
+      default:
+        //alert('portrait');
+        stereoLogo.setAttribute('material',"opacity","0");
+        logo.style.opacity = 1;
+        document.querySelector('a-scene').exitVR();
+        enteredVR = false;
+        //logo.style.width = minLength/5 + "px";
+        break; 
+    }
+}
 
 function handleOrientation(event) {
   var xpos = event.beta;  // In degree in the range [-180,180]
@@ -34,6 +54,9 @@ function handleOrientation(event) {
     }
   }
 }
+
+window.addEventListener('orientationchange', doOnOrientationChange);
+
 document.querySelector('a-scene').addEventListener('enter-vr', function () {
    //document.querySelector('a-scene').enterVR();
         stereoLogo.setAttribute('material',"opacity","1");
