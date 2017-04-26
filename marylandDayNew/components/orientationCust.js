da40d6637ecbed7b493edbbd4e01fc4fbc38ec18
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-04-26 15:33:23 -0400

    university logo in vr mode

--- marylandDayNew/components/orientationCust.js
+++ marylandDayNew/components/orientationCust.js
@@ -155,12 +155,12 @@ function handleOrientation(event) {
               document.querySelector('.a-canvas').style.left = 0;
             }
             if(landscapeModeReload){
-              var widthLandscape = document.querySelector('.a-canvas').style.width;
-              var widthLandscapeCanvas = document.querySelector('.a-canvas').width
-              document.querySelector('.a-canvas').style.width = document.querySelector('.a-canvas').style.height;
-              document.querySelector('.a-canvas').style.height = widthLandscape;
-              document.querySelector('.a-canvas').width = document.querySelector('.a-canvas').height;
-              document.querySelector('.a-canvas').height = widthLandscapeCanvas;
+              //var widthLandscape = document.querySelector('.a-canvas').style.width;
+              //var widthLandscapeCanvas = document.querySelector('.a-canvas').width
+              document.querySelector('.a-canvas').style.width = document.querySelector('.a-canvas').width/2;
+              document.querySelector('.a-canvas').style.height = document.querySelector('.a-canvas').height/2;
+              //document.querySelector('.a-canvas').width = document.querySelector('.a-canvas').height;
+              //document.querySelector('.a-canvas').height = widthLandscapeCanvas;
               document.querySelector('.a-canvas').style.left = 0;
             }
           }, 300);
