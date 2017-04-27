085b2f0ebbbd3bfd4ecbe1028a000b8864377e85
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-04-27 13:33:02 -0400

    university logo in vr mode

--- marylandDayNew/components/orientationCust.js
+++ marylandDayNew/components/orientationCust.js
@@ -15,69 +15,28 @@ logo.style.bottom = minLength/100 + "px";
 var enteredVR = false;
 var isLandscapeVRModeFirst = false;
 var landscapeModeReload = false;
-var stringCode = "data/output_webgl_"
-var leftImage = document.querySelector('#tex1');
-var rightImage = document.querySelector('#tex2');
-leftImage.setAttribute("src", stringCode+"1.png");
-rightImage.setAttribute("src", stringCode+"3.png");
 if(Math.abs(window.orientation)==90){
       landscapeModeReload = true;
 }
 document.querySelector('#loading').style.opacity = 0;
 document.querySelector('#loadingCircle').style.opacity = 0;
-/*
-var leftDiv = document.createElement("a-entity");
-leftDiv.id = "left-eye";
-leftDiv.setAttribute("geometry","primitive","plane");
-leftDiv.setAttribute("material","src: #tex1; side: double; shader: flat; transparent: true; opacity: 1");
-leftDiv.setAttribute("position","0.63 1.85 -1.2");
-leftDiv.setAttribute("rotation","0 -25 0");
-leftDiv.setAttribute("stereo","eye:left");
-document.querySelector('a-scene').appendChild(leftDiv);
 
-var rightDiv = document.createElement("a-entity");
-rightDiv.id = "right-eye";
-rightDiv.setAttribute("geometry","primitive","plane");
-rightDiv.setAttribute("material","src: #tex1; side: double; shader: flat; transparent: true; opacity: 1");
-rightDiv.setAttribute("position","0.63 1.85 -1.2");
-rightDiv.setAttribute("rotation","0 -25 0");
-rightDiv.setAttribute("stereo","eye:right");
+var stringCode = "data/output_webgl_"
+var leftImage = document.querySelector('#tex1');
+var rightImage = document.querySelector('#tex2');
+leftImage.setAttribute("src", stringCode+"3.png");
+rightImage.setAttribute("src", stringCode+"4.png");
 
-document.querySelector('a-scene').appendChild(rightDiv);
-*/
-/*
-var left = document.querySelector('#left-eye');
-var right = document.querySelector('#right-eye');
-
-left.object3D.scale.x = 2;
-left.object3D.scale.y = 2;
-left.object3D.scale.z = 2;
-right.object3D.scale = [2];
-*/
-
-function doOnOrientationChange() {
-    
+function doOnOrientationChange() {    
     switch(window.orientation) {  
       case -90 || 90:
-        
         break;
-        
-      // to automatically exit vr mode on portrait mode - for iOS
+        // to automatically exit vr mode on portrait mode - for iOS
       default:
         if(getMobileOperatingSystem()=="iOS"){
           document.querySelector('a-scene').exitVR();
         }
-        //alert('portrait');
-        /*
-        stereoLogo.setAttribute('material',"opacity","0");
-        logo.style.opacity = 1;
-        document.querySelector('a-scene').exitVR();
-        enteredVR = false;
-        //logo.style.width = minLength/5 + "px";
-        break; 
-        */
     }
-    
 }
 var count = 0;
 function handleOrientation(event) {
@@ -88,12 +47,6 @@ function handleOrientation(event) {
   if(Math.abs(xpos)<10 || Math.abs(xpos)>170){
     // keeps record the phone is brought to landscape mode after vr mode on - for android
     if(enteredVR){
-      //document.querySelector('a-scene').enterVR();
-      //stereoLogo.setAttribute('material',"opacity","1");
-      //stereoLogo.setAttribute('position',"-"+width*5.5/10000+ " -"+height*5/10000+" -0.25");
-      //logo.style.opacity =0;
-      //vrbutton.click();
-      //enteredVR = true;
       isLandscapeVRModeFirst = true;   
       //------------ check - because this will make the top as 0 again in the landscape - landscape stereo mode
       if(getMobileOperatingSystem()=="iOS"){
@@ -113,25 +66,6 @@ function handleOrientation(event) {
             document.querySelector('.a-canvas').style.left = widthcanvas * (-0.04/2);
           }, 300);
     }
-    /*
-    if(count==10){
-      var widthScreen = parseInt(document.documentElement.clientWidth);
-      var widthcanvas = parseInt(document.querySelector('.a-canvas').style.width);
-      //document.querySelector('.a-canvas').style.width = widthcanvas;
-      if(widthScreen==widthcanvas && enteredVR){
-        setTimeout(function(){ 
-          document.querySelector('.a-canvas').style.width = widthcanvas * 1.04;
-          document.querySelector('.a-canvas').style.left = widthcanvas * (-0.04/2);
-          if(getMobileOperatingSystem()=="iOS"){
-          document.querySelector('.a-canvas').style.top = minLength * (-0.15);}
-        }, 300);
-      }
-      count=0;
-    }
-    else{
-      count++;
-    }
-    */
   }
   // to automatically exit vr mode on portrait mode - for android
   if(Math.abs(xpos)>60 && Math.abs(xpos)<120){
@@ -144,38 +78,6 @@ function handleOrientation(event) {
       enteredVR = false;
       isLandscapeVRModeFirst = false;
       firstStretch = false;
-
-
-
-      /*
-
-      if(getMobileOperatingSystem()=="iOS"){
-          setTimeout(function(){ 
-            if(!enteredVR && !landscapeModeReload){
-              document.querySelector('.a-canvas').style.width = width;
-              document.querySelector('.a-canvas').style.height = height;
-              document.querySelector('.a-canvas').width = 3*width;
-              document.querySelector('.a-canvas').height = 3*height;
-              document.querySelector('.a-canvas').style.left = 0;
-            }
-            if(landscapeModeReload){
-              //var widthLandscape = document.querySelector('.a-canvas').style.width;
-              //var widthLandscapeCanvas = document.querySelector('.a-canvas').width
-              document.querySelector('.a-canvas').style.width = window.screen.availWidth;
-              document.querySelector('.a-canvas').style.height = window.screen.availWidth;
-              //document.querySelector('.a-canvas').style.width = document.querySelector('.a-canvas').width/3;
-              //document.querySelector('.a-canvas').style.height = document.querySelector('.a-canvas').height/3;
-              //document.querySelector('.a-canvas').width = document.querySelector('.a-canvas').height;
-              //document.querySelector('.a-canvas').height = widthLandscapeCanvas;
-              document.querySelector('.a-canvas').style.left = 0;
-            }
-          }, 300);
-        }
-
-*/
-
-
-
     }
   }
 }
@@ -205,10 +107,7 @@ document.querySelector('a-scene').addEventListener('exit-vr', function () {
         enteredVR = false;
 });
 window.addEventListener('deviceorientation', handleOrientation);
-
 }
-
-
 
 function getMobileOperatingSystem() {
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
