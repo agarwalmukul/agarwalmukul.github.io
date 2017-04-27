d07bc07f749184497cee8a1b588f5820c21943a1
Mukul Agarwal <mukul_bhl@yahoo.co.in>
2017-04-27 15:06:25 -0400

    university logo in vr mode

--- marylandDayNew/components/orientationCust.js
+++ marylandDayNew/components/orientationCust.js
@@ -23,7 +23,8 @@ document.querySelector('#loadingCircle').style.opacity = 0;
 
 //code ending will look like this xyz.webgl
 var url = window.location.href;
-var code = url.substring(-9,3);
+var code = url.slice(-9);
+code = code.substring(0,3);
 var stringCode = "images/webgl/" + code + "_output_webgl_";
 //var stringCode = "data/VG1_output_webgl_"
 var leftImage = document.querySelector('#left-eye');
